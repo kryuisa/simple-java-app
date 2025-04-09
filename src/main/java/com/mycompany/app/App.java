ad6784fb4cec65ae1d768a644e1e84a1e09b0182
YHUI <alisayap00@gmail.com>
2025-04-09 16:54:19 +0800

    Make a change to demonstrate Poll SCM

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Jenkins is exciting...");
+        System.out.println("Jenkins is exciting... so cool");
     }
 
     public String getMessage() {
