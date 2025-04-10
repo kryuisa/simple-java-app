569777f18288deae805ff1dfdb92f68854ceeb10
YHUI <alisayap00@gmail.com>
2025-04-10 14:51:55 +0800

    testing

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("I don't like jenkins....");
+        System.out.println("I like jenkins....");
     }
 
     public String getMessage() {
